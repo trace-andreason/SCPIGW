4419d350a4a04fd0ea1b052770e03429a0b7f072
Trace Andreason <trace.andreason@nike.com>
2018-11-11 14:29:20 -0800

    inital commit

---
+++ GMOD.groovy
@@ -0,0 +1,26 @@
+/* 
+┌ This Lightning was jarred in Portland, OR by Trace Andreason┐
+│ [===][===][===][===][===][===][===][===][===][===][===][===]│
+│ │ ϟ ││ ϟ ││ ϟ ││ ϟ ││ ϟ ││ ϟ ││ ϟ ││ ϟ ││ ϟ ││ ϟ ││ ϟ ││ ϟ ││
+│ └───┘└───┘└───┘└───┘└───┘└───┘└───┘└───┘└───┘└───┘└───┘└───┘│
+└─────────────────────────────────────────────────────────────┘
+*/
+
+class GMOD{
+    def message
+    def exchange
+    def camelContext
+    GMOD(message) {
+        this.message = message
+        this.exchange = message.exchange
+        this.camelContext = this.exchange.getContext()
+    }
+
+    def getPayloadBody() {
+        if (this.message.getBody().getClass() == java.lang.String) {
+            return message.getBody()
+        }
+        return message.getBody(java.lang.String)
+        
+    }
+}
